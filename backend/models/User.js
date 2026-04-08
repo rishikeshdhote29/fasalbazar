ea028d8e4af7b5cd049d8583472c883839730f41
Rishikesh Dhote <rishikesh.cybersec@gmail.com>
2026-04-09 02:03:09 +0530

    my profile is added

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,6 +15,10 @@ const sellerSchema = new mongoose.Schema({
 		default: "https://www.bing.com/ck/a?!&&p=89927bf3530e952584db7672f3bf60b720e4637e3cee17943a9d9241f40fb610JmltdHM9MTc3NDY1NjAwMA&ptn=3&ver=2&hsh=4&fclid=20b29e04-3a19-6056-1162-892c3bb16131&u=a1L2ltYWdlcy9zZWFyY2g_cT1wcm9maWxlK2ltYWdlK2ljb24maWQ9MzgyRTk5REZDNDMyOEREM0Q2ODk5NDgyRkMxNzRBMkY1MzFERTgwRCZGT1JNPUlBQ0ZJUg&ntb=1"
 	}
 	,
+	mobileNo:{
+		type:String,
+		default:null,
+	},
 	address:{
 		type:String,
 		default:null,
@@ -29,9 +33,7 @@ const sellerSchema = new mongoose.Schema({
 							ref:"Cart",
 							default:null
 		}
-	,mobileNo:{
-		type:String
-	}
+	
 	,order:[
 		{
 			  type:mongoose.Schema.Types.ObjectId,
