5696d642eebb249f7ab84fba90cdd8c4dbc1e549
Rishikesh Dhote <rishikesh.cybersec@gmail.com>
2026-05-09 17:51:45 +0530

    fixed

--- frontend/src/component/auth/Home.jsx
+++ frontend/src/component/auth/Home.jsx
@@ -4,7 +4,7 @@ import {useNavigate} from "react-router";
 const UserProfile = () => {
     const navigate = useNavigate();
     return (
-        <div className='h-screen  relative w-full bg-[url("/img.png")] bg-no-repeat bg-cover'>
+        <div className='h-screen  relative w-full bg-[url("/img.jpg")] bg-no-repeat bg-cover'>
 
             <p className={"ms-10 pt-5 text-white text-8xl"}>FasalBazar</p>
             <p className={"text-lg text-white ms-10 pt-5"}>A online platform for buy crops direct from farmer </p>
